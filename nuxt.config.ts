8dd6c6b54a717d0cd104b0a7ae753b8682ef14b9
Icedevil777 <icedevil777228@mail.ru>
2025-03-26 21:18:48 +0300

    # moved  to scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/sass/main.sass'],
+  css: ['~/assets/scss/main.scss'],
   modules: ['@nuxtjs/stylelint-module'],
+  stylelint: {
+    lintOnStart: false,
+    // fixOnStart: true,
+  },
 });
